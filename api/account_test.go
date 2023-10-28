06b35083c8bc5785c8ecade7796098bf4077f549
ho3eintry <ho3eintry@gmail.com>
2023-10-28 10:39:01 +0330

    Implement login user API that returns PASETO or JWT access token in Go

--- api/account_test.go
+++ api/account_test.go
@@ -82,7 +82,7 @@ func TestGetAccountAPI(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/account/%d", tc.accountID)
--- api/server.go
+++ api/server.go
@@ -1,33 +1,55 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	simpleBankDB "simpleBank/db/sqlc"
+	"simpleBank/token"
+	"simpleBank/util"
 )
 
 type Server struct {
-	store  simpleBankDB.Store
-	router *gin.Engine
+	config     util.Config
+	store      simpleBankDB.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store simpleBankDB.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store simpleBankDB.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	router.POST("/account", server.createAccount)
-	router.GET("account/:id", server.getAccount)
-	router.GET("/account", server.listAccount)
+	server.setupRouter()
+	return server, nil
+}
 
-	router.POST("/transfer", server.createTransfer)
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
-	return server
 }
 
 func (server *Server) Start(address string) error {
